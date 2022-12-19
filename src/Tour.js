41ec966f0d127eb716083af7a9ca67b54aed4b5b
SadiKISLAR <sadi22kislar@gmail.com>
2022-12-19 12:10:54 +0300

    button added

--- src/Tour.js
+++ src/Tour.js
@@ -1,7 +1,17 @@
 import React, { useState } from 'react';
 
-const Tour = () => {
-  return <h2>single tour component</h2>;
+const Tour = ({ id, image, info, price, name }) => {
+  return <article className='single-tour'>
+    <img src={image} alt={name} />
+    <footer>
+      <div className="tour-info">
+        <h4>{name}</h4>
+        <h4 className="tour-price">${price}</h4>
+      </div>
+      <p>{info}</p>
+      <button className='delete-btn'>delete tour</button>
+    </footer>
+  </article>;
 };
 
 export default Tour;
