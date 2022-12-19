39efe1fe821c3c61dc072ee4389d56c11d95f6fe
SadiKISLAR <sadi22kislar@gmail.com>
2022-12-19 11:39:10 +0300

    fitrst commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react'
+import Loading from './Loading'
+import Tours from './Tours'
+
+const url = 'https://course-api.com/react-tours-project'
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+  if (loading) {
+    return <main>
+      <Loading />
+    </main>
+  };
+  return <h2>Tours Project Setup</h2>
+}
+
+export default App
